cd619f77561c9d7121071259c8939fc961e8387b
bbgard <gonzogardiner@gmail.com>
2024-08-24 11:13:57 +1000

    updated srcs

--- public/scripts.js
+++ public/scripts.js
@@ -1,33 +1,3 @@
-// document.getElementById('fetch-button').addEventListener('click', function() {
-//   const url = document.getElementById('url-input').value;
-//   if (!url) {
-//       alert('Please enter an article URL.');
-//       return;
-//   }
-
-// //   fetch('http://localhost:5000/fetch-article', {
-//   fetch('https://courier-scraper-web-backend.vercel.app/fetch-article', {
-//       method: 'POST',
-//       headers: {
-//           'Content-Type': 'application/json'
-//       },
-//       body: JSON.stringify({ url: url })
-//   })
-//   .then(response => response.json())
-//   .then(data => {
-//       if (data.title && data.content) {
-//           document.getElementById('title').innerText = data.title;
-//           document.getElementById('content').innerHTML = data.content;
-//       } else {
-//           document.getElementById('title').innerText = 'No title found';
-//           document.getElementById('content').innerHTML = 'No content found';
-//       }
-//   })
-//   .catch(error => {
-//       console.error('Error:', error);
-//       alert('Failed to fetch the article.');
-//   });
-// });
 document.getElementById('fetch-button').addEventListener('click', function() {
     const url = document.getElementById('url-input').value;
 
